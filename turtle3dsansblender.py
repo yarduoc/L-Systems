d68f3be5f0dede3a9bd03db90ccfbccc97b310f9
yarduoc <alexmadrid@free.fr>
2017-12-08 17:04:52 +0100

    MAJ de la turtle

--- turtle3dsansblender.py
+++ turtle3dsansblender.py
@@ -129,6 +129,8 @@ class Euclidian_Space_Vector :
         representation = representation[:-1] + ")"
         return representation
         
+        
+        
 class Cartesian_Axes :
     
     X_axis = Euclidian_Space_Vector(3)
@@ -153,14 +155,37 @@ class Cartesian_Axes :
     def get_default_axes( tuple):
         X_vect = Euclidian_Space_Vector.get_vector(tuple)
         x,y,z = tuple
-        Y_vect = Euclidian_Space_Vector.get_vector((x,y,-(x**2+y**2)/z))
-        x2,y2,z2 = [int(k) for k in nditer(cross(X_vect.coordinates.transpose(),Y_vect.coordinates.transpose())))]
-        Z_vect = Y_vect = Euclidian_Space_Vector.get_vector((x2,y2,z2))
+        if x==0 :
+            Y_vect = Euclidian_Space_Vector.get_vector((1,0,0))
+        elif z==0 :
+            Y_vect = Euclidian_Space_Vector.get_vector((0,1,0))
+        elif z==0 :
+            Y_vect = Euclidian_Space_Vector.get_vector((0,0,1))
+        else :
+            Y_vect = Euclidian_Space_Vector.get_vector((x,y,-(x**2+y**2)/z))
+        x2,y2,z2 = [int(k) for k in nditer(cross(X_vect.coordinates.transpose(),Y_vect.coordinates.transpose()))]
+        Z_vect = Euclidian_Space_Vector.get_vector((x2,y2,z2))
         new_axes = Cartesian_Axes()
         new_axes.X_axis = X_vect
         new_axes.Y_axis = Y_vect
         new_axes.Z_axis = Z_vect
         return new_axes
+    
+    def get_copy_axes( axes):
+        new_axis = Cartesian_Axes()
+        new_axes.X_axis = Euclidian_Space_Vector.get_copy_vector(axes.X_vect)
+        new_axes.Y_axis = Euclidian_Space_Vector.get_copy_vector(axes.Y_vect)
+        new_axes.Z_axis = Euclidian_Space_Vector.get_copy_vector(axes.Z_vect)
+        return new_axis
+    
+    # Representation Method
+    
+    def __repr__(self):
+        representation = ""
+        representation += "ux : " + str(self.X_axis) + "\n"
+        representation += "uy : " + str(self.Y_axis) + "\n"
+        representation += "uz : " + str(self.Z_axis) + "\n"
+        return representation
         
     
     
@@ -171,46 +196,46 @@ class turtle3D :
     stored_lines        = []  
     is_down             = True      
     line_thickness      = 1
-    current_position    = Euclidian_Space_Vector(3)
-    current_orientation = Cartesian_Axes()
+    __current_position    = Euclidian_Space_Vector(3)
+    __current_orientation = Cartesian_Axes()
         
     def __init__( self, coords = (0,0,0), orientation = (1,0,0)):
         self.stored_points = []
-        self.current_position = Euclidian_Space_Vector.get_vector(coords)
-        self.current_orientation = Cartesian_Axes.get_default_axes(orientation)
+        self.__current_position = Euclidian_Space_Vector.get_vector(coords)
+        self.__current_orientation = Cartesian_Axes.get_default_axes(orientation)
     
     # Turtle execution methods 
     
     def forward( self, length=1):
-        current_position_copy = Euclidian_Space_Vector.get_copy_vector( self.current_position)
-        self.current_position.translate( self.current_orientation.X_axis.get_homothetic_vector( length))
+        current_position_copy = Euclidian_Space_Vector.get_copy_vector( self.__current_position)
+        self.__current_position.translate( self.__current_orientation.X_axis.get_homothetic_vector( length))
         if self.is_down:
-            self.stored_lines.append((current_position_copy.to_tuple(),self.current_position.to_tuple(),self.line_thickness))
+            self.stored_lines.append((current_position_copy.to_tuple(),self.__current_position.to_tuple(),self.line_thickness))
             
     def backward( self, length=1):
         self.forward(-length)
     
     def rotate_absolute_X( self, angle):
-        self.current_orientation.rotate_around_U(angle,Euclidian_Space_Vector.get_vector((1,0,0)))
+        self.__current_orientation.rotate_around_U(angle,Euclidian_Space_Vector.get_vector((1,0,0)))
         
     def rotate_absolute_Y( self, angle):
-        self.current_orientation.rotate_around_U(angle,Euclidian_Space_Vector.get_vector((0,1,0)))
+        self.__current_orientation.rotate_around_U(angle,Euclidian_Space_Vector.get_vector((0,1,0)))
         
     def rotate_absolute_Z( self, angle):
-        self.current_orientation.rotate_around_U(angle,Euclidian_Space_Vector.get_vector((0,0,1)))
+        self.__current_orientation.rotate_around_U(angle,Euclidian_Space_Vector.get_vector((0,0,1)))
         
     def rotate_relative_X( self, angle):
-        self.current_orientation.rotate_around_U(angle,self.current_orientation.X_axis)
+        self.__current_orientation.rotate_around_U(angle,self.__current_orientation.X_axis)
         
     def rotate_relative_Y( self, angle):
-        self.current_orientation.rotate_around_U(angle,self.current_orientation.Y_axis)
+        self.__current_orientation.rotate_around_U(angle,self.__current_orientation.Y_axis)
         
     def rotate_relative_Z( self, angle):
-        self.current_orientation.rotate_around_U(angle,self.current_orientation.Z_axis)
+        self.__current_orientation.rotate_around_U(angle,self.__current_orientation.Z_axis)
     
     def goto( self, coordinates):
         if self.is_down :
-            stored_lines.append((self.current_position.to_tuple(),Euclidian_Space_Vector.get_vector(coordinates).to_tuple(),self.line_thickness))
+            stored_lines.append((self.__current_position.to_tuple(),Euclidian_Space_Vector.get_vector(coordinates).to_tuple(),self.line_thickness))
         self.set_position( coordinates)
     
     def pen_up( self):
@@ -221,15 +246,83 @@ class turtle3D :
     # Methods setting parameters
     
     def set_position( self, coordinates_vector):
-        self.current_position = coordinates_vector
+        self.__current_position = coordinates_vector
     def set_orientation( self, orientation_axes):
-        self.current_orientation = orientation_axes
+        self.__current_orientation = orientation_axes
     def set_tuple_position( self, coordinates):
-        self.current_position = Euclidian_Space_Vector.get_vector(coordinates)
+        self.__current_position = Euclidian_Space_Vector.get_vector(coordinates)
     def set_tuple_orientation( self, axes):
-        self.current_orientation = Cartesian_Axes.get_default_axes(axes)
+        self.__current_orientation = Cartesian_Axes.get_default_axes(axes)
     def set_thickness(self, value):
         self.line_thickness = value
+        
+    # Getters that returns copies of attributes
+    
+    def get_position( self):
+        return Euclidian_Space_Vector.get_copy_vector( self.__current_position)
+        
+    def get_orientation( self):
+        return Cartesian_Axes.get_copy_axes( self.__current_orientation)
+        
+    # Blender
+    
+    def draw_cylinder( p1, p2, rayon):
+        x1,y1,z1 = p1[0], p1[1], p1[2]
+        x2,y2,z2 = p2[0], p2[1], p2[2]
+        dx = x2 - x1
+        dy = y2 - y1
+        dz = z2 - z1
+        dist = sqrt(dx**2 + dy**2 + dz**2)
+        bpy.ops.mesh.primitive_cylinder_add(
+            radius = rayon,
+            depth = dist,
+            location = (dx/2 + x1, dy/2 + y1, dz/2 + z1)
+        )
+        phi = arctan2(dy,dx)
+        theta = arccos(dz/dist)
+        bpy.context.object.rotation_euler[1] = theta
+        bpy.context.object.rotation_euler[2] = phi
+    
+    
+    def get_data_size( data_list):
+        (xmin,ymin,zmin) = data_list[0][0]
+        (xmax,ymax,zmax) = data_list[0][0]
+        for data in data_list:
+            ((x1,y1,z1),(x2,y2,z2),size) =  data
+            xmin = min( xmin, min( x1, x2))
+            xmax = max( xmax, max( x1, x2))
+            ymin = min( ymin, min( y1, y2))
+            ymax = max( ymax, max( y1, y2))
+            zmin = min( zmin, min( z1, z2))
+            zmax = max( zmax, max( z1, z2))
+        return max( abs( xmax-xmin), abs( ymax-ymin), abs( zmax-zmin))
+    
+    def resize_data( data_list, dimension):
+        coefficient = dimension / turtle3D.get_data_size( data_list)
+        for k in range ( len( data_list)) :
+            ((x1,y1,z1),(x2,y2,z2), size) =  data_list[k]
+            x1 *= coefficient
+            x2 *= coefficient
+            y1 *= coefficient
+            y2 *= coefficient
+            z1 *= coefficient
+            z2 *= coefficient
+            size *= coefficient
+            data_list[k] = [(x1,y1,z1),(x2,y2,z2), size]
+        return data_list
+    
+    
+    def blender_print ( self, dimension):
+        data_list = turtle3D.resize_data( self.stored_lines, dimension)
+        for data in data_list:
+            (p1,p2,size) = data
+            turtle3D.draw_cylinder(p1,p2,size)
+        
+        
+T = turtle3D()
+T.set_tuple_orientation((1,0,0))
+
+
         
         
         
