58acf2e9c48fcc16b907728da50e1fd10dd4f160
Crousthybalt <thibault.marette@gmail.com>
2018-02-13 11:02:28 +0100

    Update and rename L-system_objet-1.py to arbre_langagier.py

--- src/Essais_Arbres/arbre_langagier.py
+++ src/Essais_Arbres/arbre_langagier.py
@@ -25,20 +25,4 @@ class Arbre:
         for i in range(1,len(mot)):
             self.Noeuds.append(Noeud(mot[i],predec(mot,i,L_system),""))
 
-class Affichage:
-    
-    regle_affichage = None
-    L_system = None
-    
-    def __init__(self , regle_affichage , L_system):
-        self.regle_affichage = regle_affichage
-        self.L_system = L_system
-    
-    
-    def afficher(self,n):
-        t = turlte3D
-        graine = self.L_system.graine
-        mot = self.L_sysem.renvoie_mot(n)
-        self.regle_affichage(graine , mot , n)
-        
     
