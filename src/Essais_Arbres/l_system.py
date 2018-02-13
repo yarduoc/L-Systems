3c60d258c59b1e26d6d6cded1cc1e64150a9fd8a
Crousthybalt <thibault.marette@gmail.com>
2018-02-13 11:35:25 +0100

    Update l_system.py

--- src/Essais_Arbres/l_system.py
+++ src/Essais_Arbres/l_system.py
@@ -8,7 +8,7 @@ class L_systeme :
     
     def __init__(self,regle,graine,alphabet):
         self.regle = regle
-        self.aphabet = alphabet
+        self.alphabet = alphabet
         self.graine = graine
         self.langage.append(graine)
         
@@ -19,17 +19,32 @@ class L_systeme :
         mot = self.langage[-1]
         sortie = ""
         for lettre in mot :
-            sortie += regle(lettre)
+            sortie += self.regle(lettre)
             
         self.langage.append(sortie)
         
     def renvoyer_mot(self, n):
-        if len(self.langage) == n :
-            return self.langage[-1]
+        if len(self.langage) > n :
+            return self.langage[n]
         self.etendre_langage()
         return self.renvoyer_mot(n)
-        
-        
+    
+    def predec(self,M,k):
+        if k == 0:
+            return None
+        if M[k-1] in self.alphabet:
+            return k-1
+        if M[k-1] not in ["[","]"]:
+            return self.predec(mot,k-1)
+        j = k-1
+        while M[j] != "[" or M[j-1] not in self.alphabet:
+            j -= 1
+            if j == 0:
+                return None
+        if M[j-1] in self.alphabet:
+            return j-1
+        return self.predec(mot,j-1)
+    
 class Affichage:
     
     regle_affichage = None
@@ -42,7 +57,7 @@ class Affichage:
     
     def afficher(self,n):
         t = turlte3D
-        graine = self.L_system.graine
-        mot = self.L_sysem.renvoie_mot(n)
-        self.regle_affichage(graine , mot , n)
         
+        mot = self.L_sysem.renvoie_mot(n)
+        self.regle_affichage(mot)
+
