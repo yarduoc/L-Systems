3ad3c4990f7da7ee574165935d269e90eb34f476
Issoudab <32393360+Issoudab@users.noreply.github.com>
2018-02-23 17:24:14 +0100

    Update exemple-objet.py

--- src/L_objets/exemple-objet.py
+++ src/L_objets/exemple-objet.py
@@ -270,6 +270,28 @@ class turtle3D :
         
     # Blender
     
+    
+       
+    def draw_cylinder( p1, p2, rayon):
+        x1,y1,z1 = p1[0], p1[1], p1[2]
+        x2,y2,z2 = p2[0], p2[1], p2[2]
+        dx = x2 - x1
+        dy = y2 - y1
+        dz = z2 - z1
+        dist = sqrt(dx**2 + dy**2 + dz**2)
+        bpy.ops.mesh.primitive_cylinder_add(
+            radius = rayon,
+            depth = dist,
+            location = (dx/2 + x1, dy/2 + y1, dz/2 + z1)
+        )
+        phi = arctan2(dy,dx)
+        theta = arccos(dz/dist)
+        bpy.context.object.rotation_euler[1] = theta
+        bpy.context.object.rotation_euler[2] = phi
+        bpy.ops.mesh.primitive_uv_sphere_add(size=rayon,
+                                            location=p1)
+        bpy.ops.mesh.primitive_uv_sphere_add(size=rayon,
+                                            location=p2)
         
     def get_data_size( data_list):
         (xmin,ymin,zmin) = data_list[0][0]
@@ -304,28 +326,6 @@ class turtle3D :
         for data in data_list:
             (p1,p2,size) = data
             turtle3D.draw_cylinder(p1,p2,size)
-        
-    
-    def draw_cylinder( p1, p2, rayon):
-        x1,y1,z1 = p1[0], p1[1], p1[2]
-        x2,y2,z2 = p2[0], p2[1], p2[2]
-        dx = x2 - x1
-        dy = y2 - y1
-        dz = z2 - z1
-        dist = sqrt(dx**2 + dy**2 + dz**2)
-        bpy.ops.mesh.primitive_cylinder_add(
-            radius = rayon,
-            depth = dist,
-            location = (dx/2 + x1, dy/2 + y1, dz/2 + z1)
-        )
-        phi = arctan2(dy,dx)
-        theta = arccos(dz/dist)
-        bpy.context.object.rotation_euler[1] = theta
-        bpy.context.object.rotation_euler[2] = phi
-        bpy.ops.mesh.primitive_uv_sphere_add(size=rayon,
-                                            location=p1)
-        bpy.ops.mesh.primitive_uv_sphere_add(size=rayon,
-                                            location=p2)
         
 t = turtle3D()
 
