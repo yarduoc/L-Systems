d0e5b4ed5f34294ee687b7b1c63b146e62f639bd
Crousthybalt <thibault.marette@gmail.com>
2018-03-30 17:54:39 +0200

    super tout fonctionne presque

--- src/L_objets/l_system.py
+++ src/L_objets/l_system.py
@@ -1,24 +1,61 @@
+"""def regle(indice,mot,alphabet):
+    lettre = mot[indice]
+    if lettre == ['b'] :
+        if M.est_predecesseur(mot,indice,'a'):
+            return [['c'],['b']]
+        if M.est_predecesseur(mot,indice,'aab'):
+            return [['c'],['c'],['c']]
+        return [['a'],['a'],['a']]
+    if lettre == ['a']:
+        return [['a'],['b']]
+    return lettre
+    
+M = Morphisme(regle,['a','b'])
+
+L = L_systeme(M,['a'],['a','b'])"""
+
+
 class Morphisme:
     regle = None
-    
-    def __init__(self,regle):
+    alphabet = None
+    def __init__(self,regle,alphabet):
         self.regle = regle
+        self.alphabet = alphabet
         
-    def predec(self,mot,k,motif):
-        if k == 0:
-            return None
-        if M[k-1] in self.alphabet:
-            return k-1
-        if M[k-1] not in ["[","]"]:
-            return self.predec(mot,k-1)
-        j = k-1
-        while M[j] != "[" or M[j-1] not in self.alphabet:
-            j -= 1
-            if j == 0:
-                return None
-        if M[j-1] in self.alphabet:
-            return j-1
-        return self.predec(mot,j-1)
+    def skip_dyck(self,mot,indice):
+        compteur_dyck = -1
+        for k in range(indice-1,-1,-1):
+            if compteur_dyck == 0:
+                return k+1
+            if mot[k][0] == "[" or mot[k][0] == "]":
+                compteur_dyck += (-1)**(mot[k][0] == "]")
+        return "Impossible"
+    
+    def indice_predec(self, mot, indice):
+        if indice == 0:
+            return "Impossible"
+        if mot[indice - 1][0] in self.alphabet:
+            return indice - 1
+        elif mot[indice - 1][0] == "]":
+            j = self.skip_dyck(mot,indice - 1)
+            if j == "impossible" :
+                return "impossible"
+            return self.indice_predec(mot, j)
+        else:
+            return self.indice_predec(mot, indice - 1)
+            
+            
+    def est_predecesseur(self, mot,indice, motif):
+        if motif == "" :
+            return True
+        
+        nouvel_indice = self.indice_predec(mot,indice)
+        
+        if nouvel_indice == "Impossible" :
+            return False
+        if mot[nouvel_indice][0] != motif[-1] :
+            return False
+        return self.est_predecesseur(mot,nouvel_indice, motif[:-1:])
         
     def succ(self,mot,k,motif):
         pass
@@ -27,20 +64,23 @@ class Morphisme:
         
         sortie = []
         for k in range(len(mot)):
-            m = self.regle(mot,k,self.predec,self.succ)
-            sortie.append(m)
+            
+            m = self.regle(k,mot,self.alphabet)
+            
+            sortie += m
+
         return sortie
-        
+
 
 class L_systeme :
 
     alphabet = []
-    regle = None
+    morphism = None
     graine = ""
     langage = []
     
-    def __init__(self,regle,graine,alphabet):
-        self.regle = regle
+    def __init__(self,morphism,graine,alphabet):
+        self.morphism = morphism
         self.alphabet = alphabet
         self.graine = graine
         self.langage.append(graine)
@@ -51,7 +91,7 @@ class L_systeme :
     def etendre_langage(self):
         mot = self.langage[-1]
         
-        self.langage.append(regle.appliquer(mot))
+        self.langage.append(self.morphism.appliquer(mot))
         
     def renvoyer_mot(self, n):
         
@@ -79,8 +119,3 @@ class Interpretation_geometrique:
         
         mot = self.L_system.renvoyer_mot(n)
         self.regle_affichage(mot)
-        
-        
-
-        
-        
