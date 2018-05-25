d07923572993423a0cd4e85e78527addf45daf63
Yarduoc <alexmadrid@free.fr>
2018-05-25 14:46:43 +0200

    commit

--- src/Turtle_3D/creer_fichier.py
+++ src/Turtle_3D/creer_fichier.py
@@ -1,8 +1,8 @@
 list = t.stored_lines
-fichier = open("fichier_coordonees_arbre.txt",'w')
+fichier = open("ichier_coordonees_arbre.txt",'w')
 fichier.write('['+'\n')
 for ligne in list:
     fichier.write(str(ligne)+','+'\n')
 
 fichier.write(']'+'\n')
-fichier.close()
+#fichier.close()
